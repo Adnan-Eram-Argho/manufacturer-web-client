2beaedd634c981a47f2b440d8cc62d6f32e54260
Your Name <adnaneramargho@gmail.com>
2022-05-25 00:22:24 +0600

    banner done

---
+++ src/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+        <div >
+
+            <div>
+                <div className='Banner'>
+                    <div className='app banner-text'>
+                        <h1 className='lg:text-6xl sm:text-3xl'>We manufacturer different painting tools</h1>
+                        <p className='mt-6 text-white'>IF you need any then we are always here</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
